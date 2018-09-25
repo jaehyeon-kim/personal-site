2b39da36bd6b06a43f79882cc6b2c049eb845015
bernie <jaehyeon.kim@tal.com.au>
2018-09-25 23:10:22 +1000

    up blog/articles layout

--- src/components/blog/Article.vue
+++ src/components/blog/Article.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <h1>{{article.title}}</h1>
-    <router-link to="/blog">Back</router-link>
-    <hr/>
-    <div v-html="article.__content"></div>
-  </div>
+  <v-layout row>
+    <v-flex xs10 md8 lg6 offset-xs1 offset-md2 offset-lg3>
+      <v-toolbar color="grey lighten-2" flat class="mb-2">
+        <v-toolbar-title>
+          <div class="mt-3">
+            <span>{{ article.title }}</span>
+            <p class="caption">{{ `${article.created ? article.created.split('T')[0] : ''}`}}</p>
+          </div>          
+        </v-toolbar-title>
+        <v-btn absolute icon dark small center right color="grey darken-1" to='/blog'>
+            <v-icon size="25">navigate_before</v-icon>
+        </v-btn>        
+      </v-toolbar>      
+      <div id="scroll-target" :style="'max-height:' + (this.windowHeight - 180) + 'px'" class="scroll-y">
+        <v-layout v-scroll:#scroll-target="onScroll" column>
+          <div style="background: #EEEEEE" class="pl-2 pr-2 pt-2 pb-2" v-html="article.__content"></div>
+        </v-layout>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -19,9 +33,28 @@ export default {
   data() {
     console.log(require(`../../posts/${this.id}.md`))
     return {
+      windowHeight: 0,
+      offsetTop: 0,      
       article: require(`../../posts/${this.id}.md`), // eslint-disable-line global-require, import/no-dynamic-require
     }
-  }
+  },
+  methods: {
+    getWindowHeight(event) {
+        this.windowHeight = document.documentElement.clientHeight
+    },
+    onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+    }    
+  },
+  mounted() {
+    this.$nextTick(function() {
+        window.addEventListener('resize', this.getWindowHeight)
+        this.getWindowHeight()
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.getWindowHeight);
+  }  
 }
 </script>
 
--- src/components/blog/Blog.vue
+++ src/components/blog/Blog.vue
@@ -1,24 +1,68 @@
 <template>
-  <div>
-    <article v-for="article in articles" :key="article.slug">
-      <router-link :to="`/blog/${ article.slug }`">
-        <h2>{{ article.title }}</h2>
-        <p>{{article.description}}</p>
-      </router-link>
-    </article>
-  </div>
+  <v-layout row>
+    <v-flex xs10 md8 lg6 offset-xs1 offset-md2 offset-lg3>
+      <v-toolbar color="grey lighten-2" flat class="mb-2">
+        DO somthing
+        <v-btn absolute icon dark small center right color="grey darken-1" to='/'>
+            <v-icon size="18">home</v-icon>
+        </v-btn>          
+      </v-toolbar>
+      <div id="scroll-target" :style="'max-height:' + (this.windowHeight - 200) + 'px'" class="scroll-y">
+        <v-layout v-scroll:#scroll-target="onScroll" column>
+          <v-list two-line>
+            <template v-for="(article, index) in articles">
+              <v-list-tile
+                :key="article.slug"
+                avatar
+                ripple
+                :to="`/blog/${ article.slug }`"
+              >
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ article.title }}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{ `${article.created ? article.created.split('T')[0] : ''}`}}</v-list-tile-sub-title>
+                    <v-list-tile-sub-title class="caption">{{ article.description }}</v-list-tile-sub-title>
+                  </v-list-tile-content>                              
+                </v-list-tile>
+                <v-divider :key="index"></v-divider>
+            </template>
+          </v-list>
+        </v-layout>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import InfiniteLoading from 'vue-infinite-loading'
-
 export default {
   name: 'Blog',
+  data() {
+    return {
+      windowHeight: 0,
+      offsetTop: 0
+    }
+  },  
   computed: {
     articles() {
-      return this.$store.getters['blog/articles']
+      return this.$store.getters['blog/articles'].length > 0 ? this.$store.getters['blog/articles'] : []
     }
   },
+  methods: {
+    getWindowHeight(event) {
+        this.windowHeight = document.documentElement.clientHeight
+    },
+    onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+    }    
+  },
+  mounted() {
+    this.$nextTick(function() {
+        window.addEventListener('resize', this.getWindowHeight)
+        this.getWindowHeight()
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.getWindowHeight);
+  },  
   created() {
     this.$store.commit('blog/updateArticles')
   }
