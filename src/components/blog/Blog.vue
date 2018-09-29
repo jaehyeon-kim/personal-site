f108a833b9aa553b85908440d05f137ed12a2091
bernie <jaehyeon.kim@tal.com.au>
2018-09-29 20:36:30 +1000

    more layout updates

--- src/components/blog/Blog.vue
+++ src/components/blog/Blog.vue
@@ -1,37 +1,50 @@
 <template>
-    <v-container grid-list-sm fluid>
-        <v-layout row wrap justify-center align-center>
-                <v-card width="600" max-height="250" v-for="(article, i) in articles" :key="i" class="ml-3 mr-3 mb-3">
+    <v-container fluid grid-list-lg>
+        <v-layout row wrap>
+            <v-flex xs10 sm8 md6 offset-xs1>
+                <v-card max-height="250" v-for="(article, i) in articles" :key="i" class="mb-3">
                     <v-card-text>
-                        <div>
+                        <div @click="openArticle(article)">
                             <span class="headline">{{ article.title }}</span><br>
                             <span class="caption">{{ `${dateToString(article.created)}` }}</span><br>                      
                         </div>
                     </v-card-text>
                     <v-divider light></v-divider>
                     <v-layout>
-                        <v-flex xs5>
-                            <v-img
-                                :src="`/static/${article.slug}/main.png`"
-                                height="150"
-                                width="200"
-                                contain
-                                class="ml-3"
-                                :to="`/blog/${ article.slug }`"
-                            >
-                            </v-img>
-                        </v-flex>
-                        <v-flex xs7>
+                        <div :class="isHidden ? 'flex xs12' : 'flex xs5'">
+                            <div @click="openArticle(article)">
+                                <v-img
+                                    :src="`/static/${article.slug}/main.png`"
+                                    height="120"
+                                    contain
+                                    class="mt-1 mb-1"
+                                >
+                                </v-img>
+                            </div>
+                        </div>
+                        <v-flex xs7 class="hidden-sm-and-down">
                             <v-card-text>
-                                <span class="caption">{{ article.description.substring(0, 220) + '...' }}</span><br>
-                                <v-btn flat small block color="primary" :to="`/blog/${ article.slug }`">
-                                    read more
-                                    <v-icon>fast_forward</v-icon>
-                                </v-btn>                                
-                            </v-card-text>                       
+                                <div @click="openArticle(article)" class="caption mb-1">{{ article.description.substring(0, maxStr) + '... ' }}</div>
+                            </v-card-text>      
                         </v-flex>
                     </v-layout>
                 </v-card>
+            </v-flex>
+			<div :class="isHidden ? 'flex xs10 sm8 offset-xs1' : 'flex xs10 sm4'">
+                <v-layout column>
+                    <v-flex>
+                        <v-card height="200">
+                            First Card
+                        </v-card>
+                    </v-flex>
+                    <v-flex>
+                        <v-card height="200">
+                            Second Card
+                            {{ windowWidth}}
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+			</div>
         </v-layout>
     </v-container>
 </template>
@@ -44,9 +57,25 @@ export default {
   props: ['slug'],
   data() {
     return {
+        windowWidth: 0,
     }
   },  
   computed: {
+    isHidden() {
+        return this.windowWidth < 960
+    },
+    maxStr() {
+        if (this.windowWidth < 960) {
+            return 100
+        }
+        else if (this.windowWidth < 1264) {
+            return 220
+        } else if (this.windowWidth < 1600) {
+            return 300
+        } else {
+            return 500
+        }
+    },
     articles() {
       return this.$store.getters['blog/articles'].sort((a, b) => {
         if (a.created > b.created) return -1
@@ -57,6 +86,23 @@ export default {
     dateToString() {
       return this.$store.getters['blog/dateToString']
     }
-  }
+  },
+  methods: {
+    getWindowWidth(event) {
+        this.windowWidth = document.documentElement.clientWidth
+    },      
+    openArticle(article) {
+        this.$router.push({ path: `/blog/${ article.slug }` })
+    }
+  },
+  mounted() {
+    this.$nextTick(function() {
+        window.addEventListener('resize', this.getWindowWidth)
+        this.getWindowWidth()
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.getWindowWidth);
+  } 
 }
 </script>
